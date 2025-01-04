6fee2f0e8eba8b56c22c15c05b8c175ed13859ca
paulo <phenrique@viemed.com>
2025-01-04 14:18:34 -0300

    fixing build

--- scripts/create-version-tag.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-VERSION=$(node -p "require('./package.json').version")
-git tag -a "v$VERSION" -m "Release version $VERSION"
-git push origin "v$VERSION" 
--- src/common/config/version.ts
+++ src/common/config/version.ts
@@ -1,6 +1,11 @@
-import { version } from '../../../package.json';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const pkg = JSON.parse(readFileSync(join(__dirname, '../../../package.json'), 'utf-8'));
 
 export const VERSION = {
-  number: version,
-  apiVersion: process.env.API_VERSION || 'v1'
-};
+  number: pkg.version,
+  apiVersion: 'v1'
+} as const;
